efd03e58144afd30d1dada911cf7bc3a7a894bc1
Oscar Wooding <Oscarwooding@gmail.com>
2024-10-07 10:21:45 +0100

    new trade idea on oil

--- Systematic/strategy/momentum.py
+++ Systematic/strategy/momentum.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 
-def simple_momentum_vix(df, CAPITAL, WINDOW):
+def simple_momentum_vix(df, CAPITAL, LAYERS, WINDOW):
     """
     input: 
         data: a dict of dataframes.
@@ -12,19 +12,19 @@ def simple_momentum_vix(df, CAPITAL, WINDOW):
                 All DataFrames are: (Time_Open(index), markets...) 
         CAPITAL: maximum amount of capital to deploy
         WINDOW: window used in determining momentum
+        LAYERS: number of equal sized trades to make before reaching capital limit
     output:
         desired_positions = { date: DateTime, DateFrame}
         desired_position = desired_position_normalized * capital
 
     """
 
-    cap = CAPITAL / 4
-
     # Get trades
     df['R'] = df['Close'].pct_change()
     R = df['R']
-    signal = np.sign(R.rolling(WINDOW).mean())
-    final_positions_usd = CAPITAL/10 * signal
+    signal_buy = np.sign(R.rolling(WINDOW).mean())
+
+    final_positions_usd = CAPITAL/LAYERS * signal_buy
     final_positions_usd = final_positions_usd.fillna(0)
 
     # Pre allocate
@@ -32,7 +32,9 @@ def simple_momentum_vix(df, CAPITAL, WINDOW):
     pos = np.zeros(len(final_positions_usd))
 
     for i, trade in enumerate(final_positions_usd): 
-        if (abs(total_pos + trade) <= cap) and ((total_pos + trade) >= 0) : 
+        if trade < 0: 
+            total_pos = 0
+        if (abs(total_pos + trade) <= CAPITAL) and ((total_pos + trade) >= 0) : 
             total_pos += trade
         else: 
             None
